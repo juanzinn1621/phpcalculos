7a7ad17898f09790734bba1368d63f5e99406bec
juanzinn1621 <127741820+juanzinn1621@users.noreply.github.com>
2024-03-19 09:31:43 -0300

    Add files via upload

---
+++ salario.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calcula e exibe o aumento</title>
+</head>
+<body>
+    <?php
+    $_nome = $_POST["nome"];
+    echo "Nome do(a) Funcionário(a): " . $_nome . "<br>";
+    $_data = $_POST["data_ad"];
+    echo "Data de Admissão:" . $_data . "<br>";
+    $_salario = $_POST["salario"];
+    echo "Salário: " . $_salario . "<br>";
+    $_tele = $_POST["telefone"];
+    echo "Contato: " . $_tele . "<br>";
+    $_aumento = 234;
+    echo "Aumento: " . $_aumento . "<br>";
+    $_soma = $_salario + $_aumento;
+    echo "O salário de " . $_nome . " com aumento é: " . $_soma . "<br>";
+    ?>
+</body>
+</html>
